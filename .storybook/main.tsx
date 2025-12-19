7af141107909d83cae44a67887a0af5365b1d37a
Thijs de Mooij <thijs.de.mooij@eurail.com>
2025-12-19 13:40:10 +0100

    Update Storybook to framework-based configuration
    - Update all story files to use @storybook/react-vite imports
    - Update Storybook config files to use framework-specific types
    - Remove .env-rename file
    - Update package dependencies

--- .storybook/main.tsx
+++ .storybook/main.tsx
@@ -1,5 +1,10 @@
+// This file has been automatically migrated to valid ESM format by Storybook.
+import { fileURLToPath } from "node:url";
 import type { StorybookConfig } from "@storybook/react-vite";
-import path from "path";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const config: StorybookConfig = {
   stories: [
@@ -7,11 +12,7 @@ const config: StorybookConfig = {
     "../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
 
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-docs"],
 
   framework: {
     name: "@storybook/react-vite",
